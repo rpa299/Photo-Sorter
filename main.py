80bec81176b1604bf6775d28300b1ab2642c01af
rpa299 <rpalmeida299@gmail.com>
2024-02-20 10:41:05 +0000

    Check the date of the photos

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello-World')
+from exif import Image
+import os
+
+images = os.listdir('photos')
+for image in images:
+    with open(f'photos/{image}','rb') as image_file:
+        image = Image(image_file)
+        print(image.datetime)
